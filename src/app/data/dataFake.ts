308e516cf443b2639e0ac276ac54059d711bff1a
Lucas <super.ghast8@gmail.com>
2023-12-15 06:13:31 -0300

    add content pages dynamics

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Novo Episodio de Jujutsu Kaisen acaba de ser lançado",
+    "description": "Um dos episódios mais aguardados pelos fans do anime acaba de ser lançado!",
+    "photoCover": "https://sm.ign.com/t/ign_latam/screenshot/default/jujutsu-kaisen-final-manga2023_2dta.1280.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Novo arco de One Piece está no ar!",
+    "description": "bla bla bla",
+    "photoCover": "https://topmovies.com.br/wp-content/uploads/2023/11/One-Piece-Anime-revela-trailer-do-arco-Egghead-e-data.jpg.webp"
+  },
+  {
+    "id":"3",
+    "title": "O mangá de Chainsaw Man está ficando chato?",
+    "description": "bla bla bla",
+    "photoCover": "https://cabanadoleitor.com.br/wp-content/uploads/2023/08/chainsaw-man-1536x864.webp"
+  },
+  {
+    "id":"4",
+    "title": "Estudio MAPPA está sobrecarregando seus artistas...",
+    "description": "bla bla bla",
+    "photoCover": "https://multiversonoticias.com.br/wp-content/uploads/2023/10/mappa.jpg"
+  }
+
+]
